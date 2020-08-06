ea46ab1e59307d461d5af7b1bc31e523bebf1bf6
Jooyoung <pyoungjy@gmail.com>
2020-08-07 05:00:36 +0900

    static / media 적용

--- Untitled/Untitled/settings.py
+++ Untitled/Untitled/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#ijy@s&ga_hp#yx1^2v$47#ie+w9704a*z($u^20b+o76k6vq!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -121,3 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'Books', 'static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
