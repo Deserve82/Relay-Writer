a116500ca6c9cd71b532c8b798ffa8b8d297fb29
Jooyoung <pyoungjy@gmail.com>
2020-08-21 03:05:22 +0900

    model 카테고리 적용

--- Untitled/Books/models.py
+++ Untitled/Books/models.py
@@ -26,29 +26,30 @@ class Rate(models.Model):
     
 
 class Novel(models.Model):
-    category = (                    # novel 카테고리
-                ("세계문학전집"),
-                ("인문"),
-                ("철학"),
-                ("사회"),
-                ("에세이"),
-                ("과학"),
-                ("역사"),
-                ("경제경영"),
-                ("자기계발"),
-                ("여행"),
-                ("라이프스타일"),
-                ("부모"),
-                ("어린이/청소년"),
-                ("종교"),
-                ("판타지/무협"),
-                ("로맨스"),
-                ("만화"),
-                ("애니메이션"),
+    category = (
+        ("세계문학전집", "세계문학전집"),
+        ("인문", "인문"),
+        ("철학", "철학"),
+        ("종교", "종교"),
+        ("사회", "사회"),
+        ("에세이", "에세이"),
+        ("과학", "과학"),
+        ("역사", "역사"),
+        ("경제경영", "경제경영"),
+        ("자기계발", "자기계발"),
+        ("여행", "여행"),
+        ("라이프스타일", "라이프스타일"),
+        ("부모", "부모"),
+        ("어린이/청소년", "어린이/청소년"),
+        ("판타지/무협", "판타지/무협"),
+        ("로맨스", "로맨스"),
+        ("만화", "만화"),
+        ("애니메이션", "애니메이션"),
     )
     title = models.CharField(max_length=30)        # novel 제목 # max_length=30으로 변경
     author = models.CharField(max_length=30)       # novel 저자 -> book은 엮은 이가 무조건 User기 때문에 문제가 없늗데, novel은 저자가 User가 아닌 고전 작가일수도, User일수도 있지 않나? 그럼 우카징ㅠ
-    publisher = models.CharField(max_length=30)    # novel 출판사 -> 없을 수도 있음
+    novelCategory = models.CharField(max_length = 50, choices=category, default = '')
+    publisher = models.CharField(max_length=30, blank=True)    # novel 출판사 -> 없을 수도 있음
     writingDate = models.DateField(default=datetime.date.today)                                         # novel 작성일
     novelImage = models.ImageField(upload_to="novel/")        # novel 표지
     novelPrice = models.IntegerField(null = True,default=0)                  # novel 가격
@@ -56,28 +57,29 @@ class Novel(models.Model):
     # tag = models.CharField(max_length=20)
 
 class Book(models.Model):
-    category = (                    # book 카테고리
-                ("세계문학전집"),
-                ("인문"),
-                ("철학"),
-                ("사회"),
-                ("에세이"),
-                ("과학"),
-                ("역사"),
-                ("경제경영"),
-                ("자기계발"),
-                ("여행"),
-                ("라이프스타일"),
-                ("부모"),
-                ("어린이/청소년"),
-                ("종교"),
-                ("판타지/무협"),
-                ("로맨스"),
-                ("만화"),
-                ("애니메이션"),
+    category = (
+        ("세계문학전집", "세계문학전집"),
+        ("인문", "인문"),
+        ("철학", "철학"),
+        ("종교", "종교"),
+        ("사회", "사회"),
+        ("에세이", "에세이"),
+        ("과학", "과학"),
+        ("역사", "역사"),
+        ("경제경영", "경제경영"),
+        ("자기계발", "자기계발"),
+        ("여행", "여행"),
+        ("라이프스타일", "라이프스타일"),
+        ("부모", "부모"),
+        ("어린이/청소년", "어린이/청소년"),
+        ("판타지/무협", "판타지/무협"),
+        ("로맨스", "로맨스"),
+        ("만화", "만화"),
+        ("애니메이션", "애니메이션"),
     )
     title = models.CharField(max_length=150)                            # book 제목
     editor = models.ForeignKey(User, on_delete=models.CASCADE)          # 엮은이
+    bookCategory = models.CharField(max_length = 50, choices=category, default = '')
     editDate = models.DateField()                                       # book 엮은 날짜
     bookImage = models.ImageField(upload_to="book/")                    # book 표지
     bookPrice = models.IntegerField(null = True,default=0)              # book 가격
