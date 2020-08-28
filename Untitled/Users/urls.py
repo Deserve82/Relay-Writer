44f95997310be91bdd5e4c8c66176bf414c1cb25
Jooyoung <pyoungjy@gmail.com>
2020-08-28 21:37:25 +0900

    navbar 정리

--- Untitled/Users/urls.py
+++ Untitled/Users/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path("signin", views.signin, name="signin"),
-    path("logout", views.signin, name="logout"),
+    path("logout", views.logout_view, name="logout"),
     path("signup", views.signup, name="signup"),
 ]
